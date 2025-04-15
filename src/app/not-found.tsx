8f87321d69d3438133fa0ddc32591308fbecfa87
jakub <jakub.king1998@gmail.com>
2025-04-15 23:33:27 +0200

    add change position to main path

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+export default function NotFound() {
+  useEffect(() => {
+    // Przekieruj na stronę główną
+    redirect("/");
+  }, []);
+
+  // Możesz opcjonalnie zwrócić null lub jakiś komunikat ładowania,
+  // ale przekierowanie nastąpi niemal natychmiast.
+  return null;
+  // Lub np.:
+  // return <p>Przekierowywanie na stronę główną...</p>;
+}
